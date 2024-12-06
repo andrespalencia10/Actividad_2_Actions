b8f0ce142090a4072fffcb9223895315d26a8e8a
sebastianpalencia23 <apalencialo02mail.com>
2024-12-05 23:49:00 -0500

    refactoring url

--- main.py
+++ main.py
@@ -1,21 +1,13 @@
-import argparse
+import time
 import requests
+from requests.exceptions import RequestException
 from bs4 import BeautifulSoup
 import pandas as pd
 
 class LinioScraper:
-    def __init__(self):
-        self.base_url = None
+    def __init__(self, base_url):
+        self.base_url = base_url
         self.data = []
-
-    def set_country(self, country_code):
-        urls = {
-            1: 'https://www.linio.com.ar/search?scroll=&q=',
-            2: 'https://www.linio.cl/search?scroll=&q=',
-            3: 'https://www.linio.com.co/search?scroll=&q=',
-            4: 'https://www.linio.com.mx/search?scroll=&q=',
-        }
-        self.base_url = urls[country_code]
 
     def scraping(self, product_name):
         cleaned_name = product_name.replace(" ", "+").lower()
@@ -26,49 +18,64 @@ class LinioScraper:
             page_number += 1
 
         self.data = []
-
         for i, url in enumerate(urls, start=1):
-            response = requests.get(url)
-            soup = BeautifulSoup(response.text, 'html.parser')
-            content = soup.find_all('div', class_='catalogue-product')
+            try:
+                response = self.get_url_with_retries(url)
+                soup = BeautifulSoup(response.text, 'html.parser')
+                content = soup.find_all('div', class_='catalogue-product')
 
-            if not content:
-                print("\nNo hay más contenido para scrapear.")
+                if not content:
+                    print("\nNo hay más contenido para scrapear.")
+                    break
+
+                print(f"\nScrapeando página número {i}. {url}")
+
+                for post in content:
+                    title = post.find('a', class_='title-section').text.strip()
+                    price = post.find('span', class_='price-main-md').text.strip()
+                    post_link = post.find("a", class_='title-section')["href"]
+                    post_link = "https://www.linio.com.co" + post_link
+
+                    try:
+                        img_link = post.find("img")["data-lazy"]
+                    except:
+                        img_link = post.find("img")["src"]
+
+                    post_data = {
+                        "title": title,
+                        "price": price,
+                        "post link": post_link,
+                        "image link": img_link
+                    }
+
+                    self.data.append(post_data)
+            except RequestException as e:
+                print(f"Error al realizar la solicitud a {url}: {e}")
                 break
 
-            print(f"\nScrapeando página número {i}. {url}")
-
-            for post in content:
-                title = post.find('a', class_='title-section').text.strip()
-                price = post.find('span', class_='price-main-md').text.strip()
-                post_link = post.find("a", class_='title-section')["href"]
-                post_link = "https://www.linio.com" + post_link
-
-                try:
-                    img_link = post.find("img")["data-lazy"]
-                except:
-                    img_link = post.find("img")["src"]
-
-                post_data = {
-                    "title": title,
-                    "price": price,
-                    "post link": post_link,
-                    "image link": img_link
-                }
-
-                self.data.append(post_data)
+    def get_url_with_retries(self, url, retries=3, delay=5):
+        for i in range(retries):
+            try:
+                return requests.get(url)
+            except RequestException as e:
+                print(f"Intento {i+1} fallido: {e}")
+                if i < retries - 1:
+                    print(f"Reintentando en {delay} segundos...")
+                    time.sleep(delay)
+                else:
+                    raise
 
     def export_to_csv(self):
         df = pd.DataFrame(self.data)
-        df.to_csv("linio_scraped_data.csv", sep=";")
+        df.to_csv("linio_colombia_scraped_data.csv", sep=";", index=False)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Linio Scraper")
-    parser.add_argument("--country", type=int, required=True, help="Número del país (1: Argentina, 2: Chile, 3: Colombia, 4: México)")
-    parser.add_argument("--product", type=str, required=True, help="Nombre del producto a buscar")
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--product', type=str, required=True, help="Producto a buscar")
     args = parser.parse_args()
 
-    scraper = LinioScraper()
-    scraper.set_country(args.country)
+    scraper = LinioScraper(base_url="https://www.linio.com.co/search?scroll=&q=")  # URL para Colombia
     scraper.scraping(args.product)
     scraper.export_to_csv()
